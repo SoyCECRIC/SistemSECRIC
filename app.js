1bd5ad7e2e2599a105db5b51039397dda6a1fe24
Gohe <juancarlos.gomez@ulv.edu.mx>
2025-11-28 04:00:11 -0500

    fix(inetrfaz):actuializacion de vistas

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ const app = express();
 connectDB();
 
 // Middleware para parsear JSON y formularios
-app.use(express.json()); // JSON
-app.use(express.urlencoded({ extended: true })); // Formularios
 app.use(cookieParser());
 app.use(express.json({ limit: '50mb' }));           // ← antes era 1mb
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
